03c80b25bd7d0cad56317433aa627f23810aad8d
YuriLukashenko <yuri.lukashenko@computools.com>
2023-11-16 16:33:25 +0200

    Added Seeder.

---
+++ Rolling/Data/Seeder.cs
@@ -0,0 +1,33 @@
+﻿using Rolling.Models;
+
+namespace Rolling.Data
+{
+    public class Seeder
+    {
+        public List<Input> SeedInputs(int baseValue, int count)
+        {
+            var inputs = new List<Input>();
+
+            for(var i=0; i<count; i++)
+            {
+                inputs.Add(new Input()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Value = baseValue * (i + 1)
+                });
+            }
+
+            return inputs;
+        }
+
+        public InputDto SetRollingDto(InputDto.AggregationDefinition aggregation, int slidingWindow, int baseValue, int count)
+        {
+            return new InputDto()
+            {
+                Aggregation = aggregation,
+                SlidingWindow = slidingWindow,
+                Inputs = SeedInputs(baseValue, count)
+            };
+        }
+    }
+}
--- Rolling/Program.cs
+++ Rolling/Program.cs
@@ -1,28 +1,12 @@
-﻿using Rolling.Models;
+﻿using Rolling.Data;
+using Rolling.Models;
 using Rolling.Rolling;
 
 var service = new RollingService();
+var seeder = new Seeder();
 
-var outputs = service.CalculateRollingDelta(new InputDto()
-{
-    Aggregation = InputDto.AggregationDefinition.Sum,
-    SlidingWindow = 4,
-    Inputs = new List<Input>()
-    {
-        new() {Id = Guid.NewGuid().ToString(), Value = 100},
-        new() {Id = Guid.NewGuid().ToString(), Value = 200},
-        new() {Id = Guid.NewGuid().ToString(), Value = 300},
-        new() {Id = Guid.NewGuid().ToString(), Value = 400},
-        new() {Id = Guid.NewGuid().ToString(), Value = 500},
-        new() {Id = Guid.NewGuid().ToString(), Value = 600},
-        new() {Id = Guid.NewGuid().ToString(), Value = 700},
-        new() {Id = Guid.NewGuid().ToString(), Value = 800},
-        new() {Id = Guid.NewGuid().ToString(), Value = 900},
-        new() {Id = Guid.NewGuid().ToString(), Value = 1000},
-        new() {Id = Guid.NewGuid().ToString(), Value = 1100},
-        new() {Id = Guid.NewGuid().ToString(), Value = 1200},
-    }
-});
+var rollingDto = seeder.SetRollingDto(InputDto.AggregationDefinition.Sum, 5, 100, 12);
+var outputs = service.CalculateRollingDelta(rollingDto);
 
 foreach (var output in outputs)
 {
