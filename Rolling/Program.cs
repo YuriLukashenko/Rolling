b66b0d6edf5a901f58f51cdbe16a1ef38e206ac4
YuriLukashenko <yuri.lukashenko@computools.com>
2024-01-18 22:04:38 +0200

    [Pivot] Added pivot methods

--- Rolling/Program.cs
+++ Rolling/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Rolling.Data;
+using Rolling.Rolling;
 
 var slidingWindow = 12;
 Console.WriteLine($"Sliding window: {slidingWindow}");
@@ -10,11 +11,14 @@ var runner = new TestRunner();
 //var input = seeder.SeedInputsRandom(200, 24);
 //var input = seeder.SeedInputsMonths(200, 24, new DateTime(2022, 1, 1));
 
-var measuresB2B = seeder.SeedB2B().OrderBy(x => x.Date);
-runner.Run(measuresB2B, "B2B", slidingWindow, runner.GetAllTests());
+//var measuresB2B = seeder.SeedB2B().OrderBy(x => x.Date);
+//runner.Run(measuresB2B, "B2B", slidingWindow, new List<Enums.TestDefinition>() { Enums.TestDefinition.Accumulated });
 
-var measuresB2C = seeder.SeedB2C().OrderBy(x => x.Date);
-runner.Run(measuresB2C, "B2C", slidingWindow, runner.GetAllTests());
+//var measuresB2C = seeder.SeedB2C().OrderBy(x => x.Date);
+//runner.Run(measuresB2C, "B2C", slidingWindow, runner.GetAllTests());
 
-var missing = seeder.SeedInputsMissingMonths(200, 24, new DateTime(2022, 1, 1)).OrderBy(x => x.Date);
-runner.Run(missing, "MissingMonths", 4, runner.GetAllTests());
+//var missing = seeder.SeedInputsMissingMonths(200, 24, new DateTime(2022, 1, 1)).OrderBy(x => x.Date);
+//runner.Run(missing, "MissingMonths", 4, runner.GetAllTests());
+
+
+new PivotService().Run();
---
+++ Rolling/Rolling/PivotService.cs
@@ -0,0 +1,55 @@
+﻿using Rolling.Models;
+
+namespace Rolling.Rolling
+{
+    public class PivotService
+    {
+        public void Run()
+        {
+            var data = GenerateDataForPivot();
+            var pivotList = PivotList(data);
+            DisplayPivot(pivotList);
+        }
+
+        public Dictionary<DateTime, List<double>> PivotList(List<PivotDto> inputList)
+        {
+            // Pivot the list using LINQ
+            var pivotedResult = inputList
+                .GroupBy(item => item.Date)
+                .ToDictionary(
+                    group => group.Key,
+                    group => group.Select(item => item.Value).ToList()
+                );
+
+            return pivotedResult;
+        }
+
+        public List<PivotDto> GenerateDataForPivot()
+        {
+            var result = new List<PivotDto>
+            {
+                new() { Date = DateTime.Parse("2024-01-01"), Value = 10 },
+                new() { Date = DateTime.Parse("2024-01-01"), Value = 20 },
+                new() { Date = DateTime.Parse("2024-01-02"), Value = 30 },
+                new() { Date = DateTime.Parse("2024-01-02"), Value = 40 },
+                new() { Date = DateTime.Parse("2024-01-03"), Value = 50 },
+            };
+
+            return result;
+        }
+
+        public void DisplayPivot(Dictionary<DateTime, List<double>> pivots)
+        {
+            foreach (var pivot in pivots)
+            {
+                Console.WriteLine($"Date: {pivot.Key}");
+
+                foreach (var value in pivot.Value)
+                {
+                    Console.WriteLine($"  Value: {value}");
+                }
+            }
+        }
+    }
+
+}
